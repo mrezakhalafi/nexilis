b4a9f5b15b6221f65bc770ea2f55829dd9a1708c
M Reza Khalafi <mrezakhalafi@yahoo.com>
2023-03-06 16:51:02 +0700

    Bugs fix

--- logics/get_prefs.php
+++ logics/get_prefs.php
@@ -4,26 +4,10 @@
     //ini_set('display_startup_errors', 1);
     // error_reporting(E_ALL);
 
-function dbconn()
-{
-        
-        $host = "127.0.0.1:3306";
-        $user = "nup";
-        $password = "5m1t0l_aptR";
-        $database = "palio_lite_qiosk";
-
-    $dbconn = mysqli_connect($host, $user, $password, $database);
-
-    if (mysqli_connect_errno()) {
-        echo "Koneksi database gagal : " . mysqli_connect_errno();
-    } else {
-        $dbconn->autocommit(TRUE);
-        return $dbconn;
-    }
-}
+include_once($_SERVER['DOCUMENT_ROOT'] . '/nexilis/logics/chat_dbconn.php');
+$dbconn = paliolite();
 
 $be = $_GET["be"];
-$dbconn = dbconn();
 
 // get all prefs
 $queryStr = "SELECT `KEY`, `VALUE` FROM `PREFS` WHERE `BE` = $be";
--- logics/paliobutton/php/store_payment.php
+++ logics/paliobutton/php/store_payment.php
@@ -7,79 +7,79 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/nexilis/logics/chat_dbconn.php');
 $dbconn = paliolite();
 
 if (isset($_SESSION['f_pin'])) {
-    $f_pin = $_SESSION['f_pin'];
-  } else {
-    $f_pin = $_POST['f_pin'];
-    $queryCheckFpin = "SELECT F_PIN FROM USER_LIST WHERE F_PIN = '$f_pin' OR IMEI = '$f_pin'";
-    $query = $dbconn->prepare($queryCheckFpin);
-    $query->execute();
-    $getNewFPIN = $query->get_result()->fetch_assoc();
-    $query->close();
-  
-    $f_pin = $getNewFPIN["F_PIN"];
-    $_SESSION["f_pin"] = $f_pin;
-  }
+  $f_pin = $_SESSION['f_pin'];
+} else {
+  $f_pin = $_POST['fpin'];
+  $queryCheckFpin = "SELECT F_PIN FROM USER_LIST WHERE F_PIN = '$f_pin' OR IMEI = '$f_pin'";
+  $query = $dbconn->prepare($queryCheckFpin);
+  $query->execute();
+  $getNewFPIN = $query->get_result()->fetch_assoc();
+  $query->close();
+
+  $f_pin = $getNewFPIN["F_PIN"];
+  $_SESSION["f_pin"] = $f_pin;
+}
 $method = $_POST['method'];
 $status = $_POST['status'];
-$transaction_id = md5(date('Y-m-d H:i:s') . $fpin);
+$transaction_id = md5(date('Y-m-d H:i:s') . $f_pin);
 $cart = json_decode(base64_decode($_POST['cart']));
 
 foreach ($cart as $c) {
-    # code...
-    $p_code = $c->p_code;
-    $price = $c->price;
-    $amount = $c->amount;    
-    $maxQty = $c->maxQty;   
+  # code...
+  $p_code = $c->p_code;
+  $price = $c->price;
+  $amount = $c->amount;
+  $maxQty = $c->maxQty;
 
-    // get store products
-    $query = $dbconn->prepare("SELECT SHOP_CODE FROM PRODUCT WHERE CODE = ?");
-    $query->bind_param("s", $p_code);
+  // get store products
+  $query = $dbconn->prepare("SELECT SHOP_CODE FROM PRODUCT WHERE CODE = ?");
+  $query->bind_param("s", $p_code);
+  $query->execute();
+  $merchant_id  = $query->get_result()->fetch_assoc()['SHOP_CODE'];
+  $query->close();
+
+  $finalQty = $maxQty - $amount;
+
+  // update product stock
+  $updSql = "UPDATE PRODUCT SET QUANTITY = $finalQty WHERE CODE = '$p_code'";
+  $query = $dbconn->prepare($updSql);
+  $query->execute();
+  $query->close();
+
+  // insert to purchase table
+  // check if item is Post or product
+  if (isset($c->isPost)) {
+    $query = $dbconn->prepare("INSERT INTO PURCHASE (TRANSACTION_ID, MERCHANT_ID, PRODUCT_ID, POST_ID, PRICE, AMOUNT, METHOD, FPIN) VALUES (?, ?, NULL, ?, ?, ?, ?, ?)");
+    $query->bind_param("sssiiss", $transaction_id, $merchant_id, $p_code, $price, $amount, $method, $f_pin);
     $query->execute();
-    $merchant_id  = $query->get_result()->fetch_assoc()['SHOP_CODE'];
     $query->close();
-
-    $finalQty = $maxQty - $amount;
-
-    // update product stock
-    $updSql = "UPDATE PRODUCT SET QUANTITY = $finalQty WHERE CODE = '$p_code'";
-    $query = $dbconn->prepare($updSql);
+  } else {
+    $query = $dbconn->prepare("INSERT INTO PURCHASE (TRANSACTION_ID, MERCHANT_ID, PRODUCT_ID, POST_ID, PRICE, AMOUNT, METHOD, FPIN) VALUES (?, ?, ?, NULL, ?, ?, ?, ?)");
+    $query->bind_param("sssiiss", $transaction_id, $merchant_id, $p_code, $price, $amount, $method, $f_pin);
     $query->execute();
     $query->close();
-
-    // insert to purchase table
-    // check if item is Post or product
-    if (isset($c->isPost)) {      
-        $query = $dbconn->prepare("INSERT INTO PURCHASE (TRANSACTION_ID, MERCHANT_ID, PRODUCT_ID, POST_ID, PRICE, AMOUNT, METHOD, FPIN) VALUES (?, ?, NULL, ?, ?, ?, ?, ?)");
-        $query->bind_param("sssiiss", $transaction_id, $merchant_id, $p_code, $price, $amount, $method, $fpin);
-        $query->execute();
-        $query->close();
-    } else {
-        $query = $dbconn->prepare("INSERT INTO PURCHASE (TRANSACTION_ID, MERCHANT_ID, PRODUCT_ID, POST_ID, PRICE, AMOUNT, METHOD, FPIN) VALUES (?, ?, ?, NULL, ?, ?, ?, ?)");
-        $query->bind_param("sssiiss", $transaction_id, $merchant_id, $p_code, $price, $amount, $method, $fpin);
-        $query->execute();
-        $query->close();
-    }
+  }
 }
 
 // insert to notification table
 $query_shop_owner = $dbconn->prepare("SELECT CREATED_BY FROM SHOP WHERE CODE = '$merchant_id'");
 $query_shop_owner->execute();
-$fpin_shop_owner = $query_shop_owner->get_result()->fetch_assoc()['CREATED_BY'];
+$f_pin_shop_owner = $query_shop_owner->get_result()->fetch_assoc()['CREATED_BY'];
 $query_shop_owner->close();
 
 $type = '2';
 $time = time() * 1000;
-$notif_id_user = $fpin . $time;
-$notif_id_merchant = $fpin_shop_owner . $time;
+$notif_id_user = $f_pin . $time;
+$notif_id_merchant = $f_pin_shop_owner . $time;
 $query = $dbconn->prepare("INSERT INTO USER_NOTIFICATION (NOTIF_ID, TYPE, F_PIN, ENTITY_ID, TIME) VALUES (?, ?, ?, ?, ?)");
-$query->bind_param("ssssi", $notif_id_user, $type, $fpin, $transaction_id, $time);
+$query->bind_param("ssssi", $notif_id_user, $type, $f_pin, $transaction_id, $time);
 $query->execute();
 $query->close();
 
 $query = $dbconn->prepare("INSERT INTO USER_NOTIFICATION (NOTIF_ID, TYPE, F_PIN, ENTITY_ID, TIME) VALUES (?, ?, ?, ?, ?)");
-$query->bind_param("ssssi", $notif_id_merchant, $type, $fpin_shop_owner, $transaction_id, $time);
+$query->bind_param("ssssi", $notif_id_merchant, $type, $f_pin_shop_owner, $transaction_id, $time);
 $query->execute();
 $query->close();
 
 // echo "success";
-header("location: /nexilis/pages/payment.php?f_pin=".$fpin."&id=" . $transaction_id, true, 301);
+header("location: /nexilis/pages/payment.php?f_pin=" . $f_pin . "&id=" . $transaction_id, true, 301);
